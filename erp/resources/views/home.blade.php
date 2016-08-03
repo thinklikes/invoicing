1d945eb6d01ba205d69ccd20d7def360a0accfb0
thinklikes <thinklikes@gmail.com>
2016-08-03 15:54:48 +0800

    更新home.blade.php

--- erp/resources/views/home.blade.php
+++ erp/resources/views/home.blade.php
@@ -6,11 +6,10 @@
 
     <!-- Bootstrap 樣板... -->
     @foreach ($pages as $page)
-            <p>
-                <a href="{{ $pagePresenter->getPageAction($page) }}">
-                    {{ $page->name }}
-                </a>
-            </p>
+
+            <a class="btn btn-default" href="{{ action($page->action) }}">
+                {{ $page->name }}
+            </a>
     @endforeach
     <!-- 代辦：目前任務 -->
 @endsection
