249c4e7715a800910e418c58de9af6ef0b3069ce
thinklikes <thinklikes@gmail.com>
2016-08-04 09:50:07 +0800

    sale加回來

---
+++ erp/public/assets/js/sale/billOfSale.js
@@ -0,0 +1,193 @@
+var calculator = new OrderCalculator({
+    taxEnable:true,
+    discountEnable:true,
+    class_name: {
+        quantity: 'stock_quantity',
+        discount: 'discount',
+        no_tax_price: 'stock_no_tax_price',
+        subtotal: 'stock_no_tax_amount',
+        tax_or_not: 'tax_rate_code',
+        total_no_tax_amount: 'total_no_tax_amount',
+        tax: 'tax',
+        total_amount: 'total_amount'
+    }
+});
+
+$(function() {
+    /**
+     * 綁定客戶名稱自動完成的事件
+     * @type {AjaxCombobox}
+     */
+
+    $('.company_autocomplete').AjaxCombobox({
+        url: '/company/json',
+        afterSelect : function (event, ui) {
+            $('input.company_id').val(ui.item.id);
+            $('input.company_code').val(ui.item.code);
+        },
+        response : function (item) {
+            return {
+                label: item.company_abb + ' - ' + item.company_name,
+                value: item.company_name,
+                id: item.auto_id,
+                code: item.company_code,
+            }
+        }
+    });
+
+    $('.company_code').AjaxFetchDataByField({
+        url: '/company/json',
+        field_name : 'code',
+        afterFetch : function (event, data) {
+            $('input.company_id').val(data[0].auto_id);
+            $('input.company_autocomplete').val(data[0].company_name);
+        },
+        removeIfInvalid : function () {
+            $('input.company_id').val('');
+            $('input.company_autocomplete').val('');
+        }
+    });
+    //將表單元素綁定到單據計算機的內部
+    calculator.reCreateWidgets();
+    //綁定料品品名自動完成的事件
+    rebindStockCombobox();
+    //rebindQuantityBlur();
+    rebindDeleteButton();
+
+    $('#add_a_row').click(function () {
+        //console.log($('table#detail tbody tr:last').find('button:first').attr('id'));
+        if ($('table#detail tbody tr').length > 0) {
+            var new_id = $('table#detail tbody tr:last').find('input:first').attr('name').match(/\d+/g)[0];
+        } else {
+            new_id = 0;
+        }
+
+        new_id = parseInt(new_id) + 1;
+        var html ='\
+            <tr>\
+                <td>\
+                    <button type="button" class="remove_button"><i class="fa fa-remove"></i></button>\
+                </td>\
+                <td>\
+                    <input type="text" class="stock_code" name="' + app_name + 'Detail[' + new_id + '][stock_code]" size="10">\
+                    <input type="hidden" class="stock_id" name="' + app_name + 'Detail[' + new_id + '][stock_id]">\
+                </td>\
+                <td>\
+                    <input type="text" class="stock_autocomplete" name="' + app_name + 'Detail[' + new_id + '][stock_name]">\
+                </td>\
+                <td>\
+                    <input type="text" name="' + app_name + 'Detail[' + new_id + '][discount]"\
+                        class="discount numeric" size="5">％\
+                </td>\
+                <td><input type="text" class="stock_quantity" name="' + app_name + 'Detail[' + new_id + '][quantity]" style="text-align:right;" size="5"></td>\
+                <td><input type="text" class="stock_unit" name="' + app_name + 'Detail[' + new_id + '][unit]" readonly="" size="5"></td>\
+                <td><input type="text" class="stock_no_tax_price" name="' + app_name + 'Detail[' + new_id + '][no_tax_price]" style="text-align:right;" size="10"></td>\
+                <td><input type="text" class="stock_no_tax_amount" style="text-align:right;" size="10"></td>\
+            </tr>';
+        $('table#detail tbody').append(html);
+        calculator.reCreateWidgets();
+        rebindStockCombobox();
+        //rebindQuantityBlur();
+        rebindDeleteButton();
+    });
+
+});
+
+function rebindStockCombobox() {
+    $( "input.stock_autocomplete" ).each(function () {
+        if (typeof $(this).AjaxCombobox("instance") != "undefined") {
+            $(this).AjaxCombobox('destroy');
+        }
+        //console.log($(this));
+        $(this).AjaxCombobox({
+            url: '/stock/json',
+            afterSelect : function (event, ui) {
+
+                var index = event.target.name.match(/\d+/g)[0];
+
+                $('input.stock_code:eq('+index+')').val(ui.item.code);
+                $('input.stock_id').eq(index).val(ui.item.id);
+                $('input.stock_no_tax_price').eq(index).val(ui.item.price);
+                $('input.stock_unit').eq(index).val(ui.item.unit);
+
+                calculator.setDiscountByIndex(index, '');
+                calculator.calculate();
+            },
+            response : function (item) {
+                return {
+                    label: item.code + ' - ' + item.name,
+                    value: item.name,
+                    id   : item.id,
+                    code : item.code,
+                    price : item.no_tax_price_of_sold,
+                    unit : item.unit.comment
+                }
+            }
+        });
+    });
+    $( "input.stock_code" ).each(function () {
+        if ($(this).AjaxFetchDataByField("instance")) {
+            $(this).AjaxFetchDataByField('destroy');
+        }
+        var index = $(this).index(".stock_code");
+        $(this).AjaxFetchDataByField({
+            url: '/stock/json',
+            field_name : 'code',
+            triggered_by : $('.stock_code').eq(index),
+            afterFetch : function (event, data) {
+                //var index2 = event.target.name.match(/\d+/g)[0];
+
+                $('input.stock_autocomplete').eq(index).val(data[0].name);
+                $('input.stock_id').eq(index).val(data[0].id);
+                $('input.stock_no_tax_price').eq(index).val(data[0].no_tax_price_of_sold);
+                $('input.stock_unit').eq(index).val(data[0].unit.comment);
+                calculator.setDiscountByIndex(index, '');
+                calculator.calculate();
+            },
+            removeIfInvalid : function () {
+                console.log(index);
+                $('input.stock_autocomplete').eq(index).val('');
+                $('input.stock_id').eq(index).val('');
+                $('input.stock_no_tax_price').eq(index).val('');
+                $('input.stock_unit').eq(index).val('');
+            }
+        });
+    });
+}
+/**
+ * 重新綁定料品名稱自動完成的事件
+ * @return void
+*/
+// function rebindQuantityBlur() {
+//     //輸入完數量的事件
+//     $( 'input[id^="detail_quantity"], input[id^="detail_no_tax_price"]' ).unbind('blur');
+//     $( 'input[id^="detail_quantity"], input[id^="detail_no_tax_price"]' ).blur(function () {
+//         //重新計算金額
+//         calculator.calculate();
+
+//         //將游標移動到下一個料品編號
+//         var id = $(this).attr('id').match(/\d+/g)[0];
+//         var index = $('input[id^="detail_stock_code"]').index(
+//             $('input[id="detail_stock_code_' + id + '"]')
+//         ) * 1;
+//         $('input[id^="detail_stock_code"]').get(
+//             index + 1
+//         ).focus();
+//         //$( "input[id^='detail_stock_code']" ).get(index + 1).focus();
+//         //console.log($('input[id="detail_stock_code_' + index + '"]').next('input[id="detail_stock_code_1"]').length);
+//         //$('input[id="detail_stock_code_' + index + '"]').next('input[id="detail_stock_code_1"]').focus();
+//         //$('input[id^="detail_quantity"]:eq(' + realy_index + ')').next().focus();
+//     });
+// }
+
+/**
+ * 重新綁定刪除按鈕的事件
+ * @return {void}
+ */
+function rebindDeleteButton() {
+    $('button.remove_button').unbind('click');
+    $('button.remove_button').click(function () {
+        $(this).parent().parent().remove();
+        calculator.calculate();
+    });
+}
---
+++ erp/public/assets/js/sale/receivableWriteOff.js
@@ -0,0 +1,225 @@
+var index = 0;
+
+var writeOffCalculator = new WriteOffCalculator(
+    'credit_checked', 'credit_amount', 'total_credit_amount',
+    'debit_checked', 'debit_amount', 'total_debit_amount');
+
+$(function () {
+    /**
+     * 綁定客戶名稱自動完成的事件
+     * @type {AjaxCombobox}
+     */
+    $('.company_autocomplete').AjaxCombobox({
+        url: '/company/json',
+        afterSelect : function (ecent, ui) {
+            $('input.company_id').val(ui.item.id);
+            $('input.company_code').val(ui.item.code);
+
+            getReceivableByCompanyId(ui.item.id);
+
+            setTimeout('', 100);
+
+            getReceiptByCompanyId(ui.item.id);
+
+            setTimeout('', 100);
+        },
+        response : function (item) {
+            return {
+                label: item.company_abb + ' - ' + item.company_name,
+                value: item.company_name,
+                id   : item.auto_id,
+                code   : item.company_code,
+            }
+        }
+    });
+    $('.company_code').AjaxFetchDataByField({
+        url: '/company/json',
+        field_name : 'code',
+        afterFetch : function (event, data) {
+            $('input.company_id').val(data[0].auto_id);
+            $('input.company_autocomplete').val(data[0].company_name);
+        },
+        removeIfInvalid : function () {
+            $('input.company_id').val('');
+            $('input.company_autocomplete').val('');
+        }
+    });
+});
+
+function fill_amount(index) {
+    if ($('.credit_checked:eq(' + index + ')').prop('checked')) {
+        var order_amount = $('.credit_order_amount:eq(' + index + ')').val() * 1;
+        var received_amount = $('.credit_received_amount:eq(' + index + ')').val() * 1;
+
+        $('.credit_amount:eq(' + index + ')').val(order_amount - received_amount);
+    } else {
+        $('.credit_amount:eq(' + index + ')').val('');
+    }
+}
+
+/**
+ * 透過ajax抓出銷貨單的應收帳款
+ * 1. 先清除界面上應收帳款所有的項目
+ * 2. request
+ * 3. 把資料填入
+ * @param  {string} company_id 供應商的id
+ * @return {void}
+ */
+function getReceivableByCompanyId(company_id)
+{
+    clearReceivableHtml();
+
+    $.ajax({
+        method: "POST",
+        url: '/billOfSale/json/getReceivableByCompanyId/' + company_id,
+        dataType: "json",
+        data: {
+            'company_id' : company_id,
+        },
+        success: function( data ) {
+            renderReceivableHtml(data, 'billOfSale');
+        }
+    });
+
+    //抓出銷貨退回單的應收帳款
+    $.ajax({
+        method: "POST",
+        url: '/returnOfSale/json/getReceivableByCompanyId/' + company_id,
+        dataType: "json",
+        data: {
+            'company_id' : company_id,
+        },
+        success: function( data ) {
+            renderReceivableHtml(data, 'returnOfSale');
+        }
+    });
+}
+
+/**
+ * 清除界面上應收帳款所有的項目
+ * @return {void}
+ */
+function clearReceivableHtml()
+{
+    $('table.receivable tbody tr').remove();
+}
+
+/**
+ * 資料填入應收帳款區塊
+ * @param  {json} data Json格式的應收帳款資料
+ * @param  {string} type 辨別是銷貨單還是銷貨退回單參數
+ * @return {void}
+ */
+function renderReceivableHtml(data, type)
+{
+    var content;
+    var prefix = (type == 'billOfSale') ? '銷' : '退';
+    var positive = (type == 'billOfSale') ? 1 : -1;
+    for (key in data) {
+        var date = new Date(data[key]['created_at']);
+        date = date.toISOString().substring(0, 10);
+        content += '<tr>\
+                        <td>\
+                            <input class="credit_checked" type="checkbox" name="receivableWriteOffCredit['+index+'][credit_checked]" onclick="fill_amount('+index+');writeOffCalculator.calculate();" value="1">\
+                        </td>\
+                        <td>\
+                            ' + date + '\
+                            <input class="credit_type" type="hidden" name="receivableWriteOffCredit['+index+'][credit_date]" value="' + date + '">\
+                        </td>\
+                        <td>\
+                            ' + prefix + data[key]['code'] + '\
+                            <input class="credit_type" type="hidden" name="receivableWriteOffCredit['+index+'][credit_type]" value="' + type + '">\
+                            <input class="credit_code" type="hidden" name="receivableWriteOffCredit['+index+'][credit_code]" value="' + data[key]['code'] + '">\
+                        </td>\
+                        <td>\
+                            ' + data[key]['invoice_code'] + '\
+                            <input type="hidden" name="receivableWriteOffCredit['+index+'][credit_invoice_code]" value="' + data[key]['invoice_code'] + '">\
+                        </td>\
+                        <td class="numeric">' + (data[key]['total_amount'] * positive) + '</td>\
+                        <td class="numeric">\
+                            ' + (data[key]['total_amount'] * positive - data[key]['received_amount'] * positive) + '\
+                            <input type="hidden" class="credit_order_amount" name="receivableWriteOffCredit['+index+'][credit_order_amount]" value="' + (data[key]['total_amount'] * positive) + '">\
+                            <input type="hidden" class="credit_received_amount" name="receivableWriteOffCredit['+index+'][credit_received_amount]" value="' + (data[key]['received_amount'] * positive) + '">\
+                        </td>\
+                        <td><input type="text" class="credit_amount numeric" name="receivableWriteOffCredit['+index+'][credit_amount]" onkeyup="writeOffCalculator.calculate();" size="10"></td>\
+                    </tr>';
+        index ++;
+    }
+    $('table.receivable tbody').append(content);
+}
+
+/**
+ * 清除界面上收款清單所有的項目
+ * @return {void}
+ */
+function clearReceiptHtml()
+{
+    $('table.receipt tbody tr').remove();
+}
+
+/**
+ * 透過ajax抓出收款清單
+ * 1. 先清除界面上收款清單所有的項目
+ * 2. request
+ * 3. 把資料填入
+ * @param  {string} company_id 供應商的id
+ * @return {void}
+ */
+function getReceiptByCompanyId(company_id)
+{
+    //抓出銷貨單的應收帳款
+    clearReceiptHtml();
+
+    $.ajax({
+        method: "POST",
+        url: '/receipt/json/getReceiptByCompanyId/' + company_id,
+        dataType: "json",
+        data: {
+            'company_id' : company_id,
+        },
+        success: function( data ) {
+            renderReceiptHtml(data);
+        }
+    });
+}
+
+/**
+ * 資料填入收款清單區塊
+ * @param  {json} data Json格式的收款清單資料
+ * @return {void}
+ */
+function renderReceiptHtml(data)
+{
+    var content;
+    var i = 0;
+    for (key in data) {
+        content += '<tr>\
+                        <td>\
+                            <input class="debit_checked" type="checkbox" name="receivableWriteOffDebit['+i+'][debit_checked]"  onclick="writeOffCalculator.calculate();" value="1">\
+                        </td>\
+                        <td>\
+                            ' + data[key]['receive_date'] + '\
+                            <input type="hidden" name="receivableWriteOffDebit['+i+'][debit_receive_date]" value="' + data[key]['receive_date'] + '">\
+                        </td>\
+                        <td>\
+                            ' + data[key]['code'] + '\
+                            <input class="debit_code" type="hidden" name="receivableWriteOffDebit['+i+'][debit_code]" value="' + data[key]['code'] + '">\
+                        </td>\
+                        <td>\
+                            ' + (data[key]['type'] == 'cash' ? '現金' : '票據') + '\
+                            <input type="hidden" name="receivableWriteOffDebit['+i+'][debit_type]" value="' + data[key]['type'] + '">\
+                        </td>\
+                        <td>\
+                            ' + (data[key]['check_code'] ? data[key]['check_code'] : '--') + '\
+                            <input type="hidden" name="receivableWriteOffDebit['+i+'][debit_check_code]" value="' + data[key]['check_code'] + '">\
+                        </td>\
+                        <td class="numeric">\
+                            ' + data[key]['amount'] + '\
+                            <input class="debit_amount" type="hidden" name="receivableWriteOffDebit['+i+'][debit_amount]" value="' + data[key]['amount'] + '">\
+                        </td>\
+                    </tr>';
+        i ++;
+    }
+    //console.log($('table.receipt').length);
+    $('table.receipt tbody').append(content);
+}
