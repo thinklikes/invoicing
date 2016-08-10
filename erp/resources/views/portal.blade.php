3a5881b583f366b3864fcee8f176aa6590c30737
thinklikes <thinklikes@gmail.com>
2016-08-10 15:20:45 +0800

    修正crm url

--- erp/resources/views/portal.blade.php
+++ erp/resources/views/portal.blade.php
@@ -9,7 +9,7 @@
                 進銷存系統
             </a>
 
-            <a class="btn btn-default" href="/CRM/company_system.php">
+            <a class="btn btn-default" href="{{ env('CRM_URL') }}">
                 客戶關係管理系統
             </a>
 
