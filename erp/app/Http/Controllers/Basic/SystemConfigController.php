19fc32ac560a4ce44d5efb84fec06ad15a1afb78
thinklikes <thinklikes@gmail.com>
2016-08-10 14:31:57 +0800

    [visiable] 增加資料庫檔案匯出匯入功能 [visiable] 請至進銷存首頁 > 系統 > 資料備份匯出/資料備份匯入

--- erp/app/Http/Controllers/Basic/SystemConfigController.php
+++ erp/app/Http/Controllers/Basic/SystemConfigController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers\Basic;
 
+use App;
+use Excel;
+use Carbon\Carbon;
+use Storage;
 use Option\OptionRepository;
 use App\Http\Controllers\Controller;
 use App\Contracts\FormRequestInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\MessageBag;
 use App\Http\Controllers\BasicController;
-use Excel;
-use Carbon\Carbon;
-use App;
+
 
 class SystemConfigController extends BasicController
 {
@@ -71,74 +73,146 @@ class SystemConfigController extends BasicController
         return view("$this->routeName.updateLogs", ['logs' => $git_logs]);
     }
 
+    /**
+     * 設定匯出選項的介面
+     * @return Response 匯出介面
+     */
     public function exportSettings()
     {
         return view($this->routeName.'.exportSettings');
     }
 
+    /**
+     * 開始匯出
+     * @param  Request $request 表單請求的物件
+     * @return Response           開始下載的回應
+     */
     public function export(Request $request)
     {
         if ($request->input('data_type') == 'sql') {
             return $this->saveAndExportSqlFile();
-
         } else {
 
         }
     }
 
-
+    /**
+     * 設定匯入選項的介面
+     * @return Response 帶有主機中備份存檔的選項之回應
+     */
     public function importSettings()
     {
-        return view($this->routeName.'.importSettings');
+        //找出之前匯出過的檔案
+        $files = Storage::files('exports');
+
+        $tmp = array();
+
+        foreach ($files as $file) {
+            if (preg_match('/\.sql$/', $file)) {
+                preg_match('/[\d]{8}_[\d]{6}/', $file, $matches);
+                $key = Carbon::createFromFormat('Ymd_His', $matches[0])->format('Y年m月d日 H時i分s秒');
+
+                $tmp[$key] = explode("/", $file)[1];
+            }
+        }
+
+        $files = $tmp;
+        //var_dump(glob(storage_path('exports').'/*'));
+        return view($this->routeName.'.importSettings', [
+            'files' => $files
+        ]);
     }
 
-    public function generataImportDemo()
+    /**
+     * 開始匯入
+     * @param  Request $request 表單請求的物件
+     * @return Response           開始匯入處理的回應
+     */
+    public function import(Request $request)
     {
-        $excel = Excel::create('importDemo', function ($excel) {
+        if ($request->input('file') == 'from_file') {
+            if ($request->file('upload_file')->isValid()) {
+                //如果上傳檔案是有效的
+                //上傳檔案的原始檔名
+                $filename = $request->file('upload_file')->getClientOriginalName();
+                //上傳檔案的位置
+                $path = storage_path('app/imports');
+                //將上船的檔案移動到存放的目錄
+                $request->file('upload_file')->move($path, $filename);
+                $realPath = $path.'/'.$filename;
+            } else {
+                $errors = new MessageBag(['檔案上傳失敗!!']);
+                return back()->withErrors($errors);
+            }
+        } else if($request->input('file') != '') {
+            $filename = $request->input('file');
+            $path = storage_path('app/exports');
+            $realPath = $path.'/'.$filename;
+        } else {
+            //如果沒有選檔案，回上一頁並顯示錯誤
+            $errors = new MessageBag(['您未選擇任何檔案!!']);
+            return back()->withErrors($errors);
+        }
 
-            // Set the title
-            $excel->setTitle('Import Demo');
+        return $this->importSqlFileToMysql($realPath);
 
-            // Chain the setters
-            $excel->setCreator('Alfie Kuo');
-                  //->setCompany(iconv('utf-8', 'Big5', '傑瑀企業有限公司'));
-
-            // Call them separately
-            $excel->setDescription('demo table of Data to import');
-
-            $excel->sheet('sheetName', function ($sheet) {
-                // Set top, right, bottom, left
-                // $sheet->setPageMargin(array(
-                //     0.25, 0.30, 0.25, 0.30
-                // ));
-                // $sheet->with([
-                //     array('data1', 'data2'),
-                //     array('data3', 'data4'),
-                //     array('data5', 'data6')
-                // ], null, 'A1', true);
-                $a = App::make('Company\Company')->get();
-                $sheet->fromModel($a , null, 'A1', true);
-            });
-            $excel->sheet('sheetName2', function ($sheet) {
-                // Set all margins
-                $sheet->setPageMargin(0.25);
-            });
-        })
-        ->store('xls');
-
-        //return $excel;
     }
 
+    /**
+     * 在主機上存檔後匯出
+     * @param  string $realPath 存檔的位置
+     * @return Response           開始下載的回應
+     */
     private function saveAndExportSqlFile()
     {
         $db = env('DB_DATABASE');
         $db_user = env('DB_USERNAME');
         $db_password = env('DB_PASSWORD');
-        $path = storage_path('exports');
+
+        //匯出的目錄
+        $path = storage_path('app/exports');
+        //匯出檔案的名稱
         $filename = $db.'_'.(Carbon::now()->format('Ymd_His')).'.sql';
-        // //儲存SQL檔案
-        $str = exec('mysqldump -u'.$db_user.' -p'.$db_password.' '.$db.' > '.$path.'/'.$filename);
-        // //開始下載
+        $realPath = $path.'/'.$filename;
+
+        //使用mysqldump指令來儲存SQL檔案
+        //設定為完整備份(Insert語法包含欄位名稱)，
+        //且不備份Create Table的指令
+        //產生insert 語法後，
+        //在Lock Tables的語法後面插入TRUNCATE TABLE
+        $command = "mysqldump -u".$db_user." -p".$db_password." ".$db;
+        $command .= " --no-create-info --complete-insert"; 
+        $command .= " | sed -r 's/LOCK TABLES (`[^`]+`) WRITE;/";//
+        $command .= "LOCK TABLES \\1 WRITE; TRUNCATE TABLE \\1;/g'";
+        $command .= " > ".$realPath;
+
+        exec($command, $output);
+        //開始下載
+
         return response()->download($path.'/'.$filename);
+    }
+
+    /**
+     * 將上傳檔案
+     * @param  string $realPath Sql檔案的位置
+     * @return response   回到上一頁並且顯示錯誤或成功訊息
+     */
+    private function importSqlFileToMysql($realPath)
+    {
+
+        $db = env('DB_DATABASE');
+        $db_user = env('DB_USERNAME');
+        $db_password = env('DB_PASSWORD');
+
+        $command = 'mysql -u'.$db_user.' -p'.$db_password.' '.$db.' < '.$realPath;
+        exec($command, $output);
+
+        if (count($output) == 0) {
+            $status = new MessageBag(['資料庫還原完成!!']);
+            return back()->with(['status' => $status]);
+        } else {
+            $errors = new MessageBag(['資料庫還原失敗!!']);
+            return back()->withErrors($errors);
+        }
     }
 }
--- erp/resources/views/erp/basic/system_config/exportSettings.blade.php
+++ erp/resources/views/erp/basic/system_config/exportSettings.blade.php
@@ -6,15 +6,17 @@
     <form method="POST" action="/system_config/export" target="_blank" role="form">
         {{ csrf_field() }}
         <div class="form-group">
+            {{--
             <label>選擇下載檔案格式：</label>
             <select name="data_type">
                 <option value="xls">Excel97-2003</option>
                 <option value="xlsx">Exce2007</option>
                 <option value="sql">資料庫檔案</option>
-            </select>
+            </select> --}}
+            <input type="hidden" name="data_type" value="sql">
         </div>
         <div class="form-group">
-            <button class="btn btn-default" type="submit">確認送出</button>
+            <button class="btn btn-default" type="submit">下載資料庫檔案</button>
         </div>
     </form>
 @endsection
